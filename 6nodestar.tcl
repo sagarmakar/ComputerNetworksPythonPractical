365158d9c0e3f2cf35046f0f97be56eec1875691
SagarMakar <sagar123.makar@gmail.com>
2020-10-17 22:12:48 +0530

    6 nodes star topology

---
+++ 6nodestar.tcl
@@ -0,0 +1,37 @@
+set ns [new Simulator]
+
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+proc finish {} {
+    global ns nf
+    $ns flush-trace
+    close $nf
+    exec nam out.nam &
+    exit 0
+}
+
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
+
+
+$n0 shape square
+
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n0 $n2 1Mb 10ms DropTail
+$ns duplex-link $n0 $n3 1Mb 10ms DropTail
+$ns duplex-link $n0 $n4 1Mb 10ms DropTail
+$ns duplex-link $n0 $n5 1Mb 10ms DropTail
+$ns duplex-link $n0 $n6 1Mb 10ms DropTail
+
+
+
+$ns at 5.0 "finish"
+
+$ns run
